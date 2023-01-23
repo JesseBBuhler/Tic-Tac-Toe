a2d4751aac7dfd80c7ba49c7a3dab2a1c4b15b81
isaac.wadd <isaac.wadd@gmail.com>
2023-01-23 10:46:19 -0700

    first rd of board

--- Tic-Tac-Toe/Board.cs
+++ Tic-Tac-Toe/Board.cs
@@ -6,5 +6,24 @@ namespace Tic_Tac_Toe
 {
     class Board
     {
+        // default board appearance
+
+        /*
+
+        */
+
+        // receive array from 'Driver' with markers in pos 0-8
+        
+        static string ShowBoard(string[] boardMarks)
+        {
+            string newBoard = "";
+            for (int i = 0; i < boardMarks.Length; i++)
+            {
+                if (boardMarks[i] == null || boardMarks[i] == "")
+                {
+                    boardMarks[i] = (i + 1).ToString();
+                }
+            }
+        }
     }
 }
