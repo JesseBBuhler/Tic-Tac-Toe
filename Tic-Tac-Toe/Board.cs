c6a58741d6be36f348a4da0577bc04f59890331d
isaac.wadd <isaac.wadd@gmail.com>
2023-01-25 10:28:37 -0700

    comments finished

--- Tic-Tac-Toe/Board.cs
+++ Tic-Tac-Toe/Board.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,102 +7,87 @@ namespace Tic_Tac_Toe
 {
     public class Board
     {
-        // default board appearance
+        /* 
 
-        /*
-               |      |    
-            0  |  1   |  2
-           ____|______|____
-               |      |
-            3  |  4   |  5
-           ____|______|____
-               |      |
-            6  |   7  |  8
-               |      |
+        DEFAULT BOARD APPEARANCE
 
-            */
+                 |     |      
+              1  |  2  |  3
+            _____|_____|_____
+                 |     |      
+              4  |  5  |  6
+            _____|_____|_____
+                 |     |      
+              7  |  8  |  9
+                 |     |     
+        */
 
-        // receive array from 'Driver' with markers in pos 0-8
-
+        // receive array from 'driver' (Program.cs) with markers in pos 0-8
         public string ShowBoard(string[] boardMarks)
         {
-            // start the output string of the current board
+            // initialize the output string of the current board
             string currentBoard = "";
 
-            //clean the input array
+            // clean input array, set blanks to numbers to mark blank board positions
             for (int i = 0; i < boardMarks.Length; i++)
             {
-                //set blanks to numbers
                 if (boardMarks[i] == null || boardMarks[i] == "")
                 {
                     boardMarks[i] = (i + 1).ToString();
                 }
-            } 
-            //The padding is so that the lines align with each other. The appropriate Xs and Os are inserted based on the commented gameboard above
+            }
 
-            // vvv Start of gameboard creation vvv //
+            // 'gameboard' *visual* creation - based on representation above ^^
             currentBoard += "|".PadLeft(6, ' ') + "|".PadLeft(6, ' ').PadRight(12, ' ') + "\n";
-
             currentBoard += boardMarks[0].PadLeft(3) + "|".PadLeft(3) + boardMarks[1].PadLeft(3) + "|".PadLeft(3) + boardMarks[2].PadLeft(3) + "\n";
-
             currentBoard += "_____|_____|_____\n";
-
             currentBoard += "|".PadLeft(6, ' ') + "|".PadLeft(6, ' ').PadRight(12, ' ') + "\n";
-
             currentBoard += boardMarks[3].PadLeft(3) + "|".PadLeft(3) + boardMarks[4].PadLeft(3) + "|".PadLeft(3) + boardMarks[5].PadLeft(3) + "\n";
-
             currentBoard += "_____|_____|_____\n";
-
             currentBoard += "|".PadLeft(6, ' ') + "|".PadLeft(6, ' ').PadRight(12, ' ') + "\n";
-
             currentBoard += boardMarks[6].PadLeft(3) + "|".PadLeft(3) + boardMarks[7].PadLeft(3) + "|".PadLeft(3) + boardMarks[8].PadLeft(3) + "\n";
-
             currentBoard += "|".PadLeft(6, ' ') + "|".PadLeft(6, ' ').PadRight(12, ' ') + "\n";
-            // ^^^ End of gameboard creation ^^^ //
 
-            //return the gameboard
             return currentBoard;
 
         }
 
+        // this function runs each player turn to check if someone has won
         public string CheckWinner(string[] currentBoard)
         {
-            // stores either "", "x", or "o"
-
-            //default to "" because "" = no winner.
+            // stores either "", "X", or "O"; default to "" because "" = no winner
             string winResult = "";
-            
-            //check for 3 in a row going up/down
+
+            // check up/down win
             for(int i = 0; i < 3; i++)
             {
                 if (currentBoard[i] == currentBoard[i + 3] && currentBoard[i] == currentBoard[i + 6])
                 {
-                    //Someone has won set the winner
+                    // set win result to the character that has been found 3-in-a-row
                     winResult = currentBoard[i];
                 }
             }
             
-            //check across for win
+            // check right/left win
             for(int i = 0; i<currentBoard.Length; i += 3)
             {
                 if (currentBoard[i] == currentBoard[i + 1] && currentBoard[i] == currentBoard[i + 2])
                 {
-                    // win result is the person with 3 in a row
                     winResult = currentBoard[i];
                 }
             }
 
-            //check diagonal lines
+            // check 2 possible diagonal wins
             if (currentBoard[0] == currentBoard[4] && currentBoard[0] == currentBoard[8])
             {
                 winResult = currentBoard[0];
 
-            }else if(currentBoard[2] == currentBoard[4] && currentBoard[2] == currentBoard[6])
+            } else if(currentBoard[2] == currentBoard[4] && currentBoard[2] == currentBoard[6])
             {
                 winResult = currentBoard[2];
             }
 
-            // return winner(x,o, or "")
+            // return winner: "X", "O", or "" (none)
             return winResult;
         }
     }
